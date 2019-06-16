eb9f31fd6059068ac2afaeb0b5939318276dcbe1
oz kilim <ozkilim@hotmail.co.uk>
2019-06-16 19:27:47 +0100

    uptodate

--- therapy/main/admin.py
+++ therapy/main/admin.py
@@ -1,10 +1,11 @@
 # from django.contrib import admin
+# from .models import *
 
 # # Register your models here.
 # admin.site.register(Category)
-# admin.site.register(TherapistProfile)
-# admin.site.register(PatientProfile)
-# admin.site.register(Match)
+# admin.site.register(Therapist)
+# admin.site.register(Patient)
+# # admin.site.register(Match)
 # admin.site.register(TherapySession)
 # admin.site.register(SessionLog)
-# admin.site.register(Questionnaire)
+# # admin.site.register(Questionnaire)
